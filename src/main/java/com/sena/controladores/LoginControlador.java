163ccbd87b3831396345df8770ba2b974415d287
unknown <giseel35mayo@gmail.com>
2023-11-03 11:33:09 -0500

    Registrar y Contraseña

--- src/main/java/com/sena/controladores/LoginControlador.java
+++ src/main/java/com/sena/controladores/LoginControlador.java
@@ -10,10 +10,20 @@ public class LoginControlador {
 	public String login() {
 		return "login";
 	}
-	
+
 	@GetMapping("/AdministrarPrincipal")
 	public String AdministrarPrincipal() {
 		return "AdministracionPrincipal";
+	}
+
+	@GetMapping("/registro")
+	public String registro() {
+		return "registro";
+	}
+	
+	@GetMapping("/RecuperarContra")
+	public String RecuperarContra() {
+		return "RecuperarContra";
 	}
 
 }
--- src/main/java/com/sena/controladores/PublicControlador.java
+++ src/main/java/com/sena/controladores/PublicControlador.java
@@ -3,10 +3,15 @@ package com.sena.controladores;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
 import com.sena.modelo.Publicacion;
 import com.sena.servicio.publicServicio;
 
@@ -30,7 +35,7 @@ public class PublicControlador {
 		return "FormuCrearpublic";
 	}
 
-	@GetMapping("/FormuActualizarpublic")
+	@GetMapping("/FormuActualizarpublic/{IdLugar}")
 	public String FormuActualizarpublic(Model model, @PathVariable("IdLugar") int IdLugar) {
 		model.addAttribute("ObPublic", publicServicio.buscarpublic(IdLugar));
 		return "FormuActualizarpublic";
@@ -52,4 +57,12 @@ public class PublicControlador {
 		publicServicio.ActualizarPublic(publicacion);
 		return "redirect:/PublicControlador/";
 	}
+	
+	public String guarder(@Validated @ModelAttribute Publicacion publicacion,BindingResult result,
+			Model model, RedirectAttributes attributes) {
+		if(result.hasErrors()) {
+			model.addAttribute("Public", attributes)
+		}
+	}
+	
 }
